7491986e7a5bf29dfd7e050d0a47f695057bad98
Nicolae-Marian Gartu <gartu.marian1973@gmail.com>
2022-12-07 21:58:11 +0000

    Add save button and make it functional

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const fileSelector = document.getElementById('file-selector');
+const saveButton = document.getElementById('save-button');
+const fileContents = document.getElementById('file-contents');
 
 fileSelector.addEventListener('change', loadFileIntoEditor);
+saveButton.addEventListener('click', function () {
+    downloadFile('file.md', fileContents.value);
+})
 
 async function loadFileIntoEditor(event) {
-    var fileContents = await readMarkdownFile(event.target.files[0])
-    if(fileContents) {
-        document.getElementById("file-contents").value = fileContents;
+    var fileText = await readMarkdownFile(event.target.files[0])
+    if(fileText) {
+        fileContents.value = fileText;
     } else {
         alert("Error reading the file");
     }
@@ -30,3 +35,22 @@ function isMarkdownFile(file) {
 function getFileExtension(file) {
     return extension = file.name.split('.').pop().toLowerCase();
 }
+
+async function downloadFile(filename, content) {
+    const blob = new Blob([content], { type: 'plain/text' });
+    const fileHandle = getNewFileHandle(filename);
+    const fileStream = await fileHandle.createWritable();
+    await fileStream.write(blob);
+    await fileStream.close();
+};
+
+async function getNewFileHandle(filename) {
+    const opts = {
+      types: [{
+        suggestedName: filename,
+        description: 'Markdown file',
+        accept: {'text/plain': ['.md']},
+      }],
+    };
+    return await window.showSaveFilePicker(opts);
+  }
