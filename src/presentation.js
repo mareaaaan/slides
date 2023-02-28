6763396d0c8e155482ad74b36dcf479a60ab411f
Nicolae-Marian Gartu <gartu.marian1973@gmail.com>
2023-02-28 21:34:19 +0000

    Remove legacy print function

--- src/presentation.js
+++ src/presentation.js
@@ -4,8 +4,6 @@ import RevealMarkdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight.esm.js';
 import RevealNotes from 'reveal.js/plugin/notes/notes.esm.js';
 import RevealMath from 'reveal.js/plugin/math/math.esm.js';
-import Print from 'reveal.js/js/controllers/print.js'
-
 
 const slides = document.querySelector(".slides");
 
@@ -22,16 +20,3 @@ const mutationObserver = new MutationObserver(async entries => {
 
 var config = { childList: true, subtree: true };
 mutationObserver.observe(slides, config);
-
-
-export function loadPrintableSlides(event) {
-    if(event.keyCode == 83 && event.ctrlKey) {
-        event.preventDefault();
-        var printer = new Print(deck);
-        printer.setupPDF()
-    }
-}
-
-
-document.addEventListener('keydown', loadPrintableSlides);
-
