d7dbfd448933c2e0f18db26af7d6db998d58d8fd
Nicolae-Marian Gartu <nicolae-marian.gartu@kcl.ac.uk>
2023-03-10 15:36:33 +0000

    Fix charts plugin configuration

--- src/slides.js
+++ src/slides.js
@@ -24,16 +24,18 @@ function reloadPreview() {
 
 export const deck = new Reveal( document.querySelector( '.reveal' ), {
         chart: {
-            defaults: {
+            defaults: { 
                 color: 'lightgray', // color of labels
-                borderColor: [ "rgba(20,220,220,.8)" , "rgba(220,120,120,.8)", "rgba(20,120,220,.8)" ],
-                backgroundColor: [ ["rgba(0,0,0,.8)" , "rgba(220,20,20,.8)", "rgba(20,220,20,.8)", "rgba(220,220,20,.8)", "rgba(20,20,220,.8)"] ],
-                scale: {
-                    beginAtZero: true,
+                scale: { 
+                    beginAtZero: true, 
                     ticks: { stepSize: 1 },
                     grid: { color: "lightgray" } , // color of grid lines
                 },
             },
+            line: { borderColor: [ "rgba(20,220,220,.8)" , "rgba(220,120,120,.8)", "rgba(20,120,220,.8)" ], "borderDash": [ [5,10], [0,0] ] }, 
+            bar: { backgroundColor: [ "rgba(20,220,220,.8)" , "rgba(220,120,120,.8)", "rgba(20,120,220,.8)" ]}, 
+            pie: { backgroundColor: [ ["rgba(0,0,0,.8)" , "rgba(220,20,20,.8)", "rgba(20,220,20,.8)", "rgba(220,220,20,.8)", "rgba(20,20,220,.8)"] ]},
+            radar: { borderColor: [ "rgba(20,220,220,.8)" , "rgba(220,120,120,.8)", "rgba(20,120,220,.8)" ]}, 
         },
         plugins: [ Markdown , RevealHighlight, RevealMath.KaTeX, RevealAnimate, RevealChart],
         embedded: true,
