4f731451343724f40ece498c1e4adcebcec24766
Nicolae-Marian Gartu <nicolae-marian.gartu@kcl.ac.uk>
2023-03-29 14:36:09 +0100

    Fix python test creator

--- test-md-creator.py
+++ test-md-creator.py
@@ -1,18 +1,14 @@
 import math
+import os
 
 input_file = open("test.md", "r")
 data = input_file.read()
 input_file.close()
 
-
 maximum_repeats= input("The number of times the file should be repeated: ")
-no_of_files = int(math.log(maximum_repeats, 2)) 
-
-
-for file_no in range(no_of_files):
-    file_path = "test_" + str(file_no+1) + ".md"
+for file_no in range(0, maximum_repeats + 1):
+    file_path = "test_" + str(file_no) + ".md"
     new_file = open(file_path, "w")
-    new_file.write("data" * (file_no+1))
+    new_file_data = data * file_no
+    new_file.write(new_file_data)
     new_file.close()
-
-print(data)
